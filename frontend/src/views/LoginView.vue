6615565c75bf25ac0f1d7afdbd3ae9b7ee98c727
Lou8is <lis.gpublic@gmail.com>
2023-05-30 00:31:48 +0200

    Makes the login page prettier + uses vuetify

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,29 +1,44 @@
 <script setup lang="ts">
     import Auth from '@/router/auth'
-
-    let username: string = "";
-    let password: string = "";
+    import { ref, type Ref } from 'vue'
+    
+    let username: Ref<string> = ref("");
+    let password: Ref<string> = ref("");
 
     function login() {
-        Auth.login(username, password, "admin");
+        Auth.login(username.value, password.value, "admin");
     }
 </script>
 
 <template>
-    
-    <div>
-    <h1>LOGIN</h1>
-    <form @submit.prevent="login">
-      <input v-model="username" placeholder="username" />
-      <br />
-      <br />
-      <input v-model="password" placeholder="password" type="password" />
-      <br />
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  </div>
-
+    <v-container class="fill-height">
+    <v-form @submit.prevent="login" class="v-col-12">
+        
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="12" sm="6">
+                    <v-text-field v-model="username" label="Username" required/>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="12" sm="6">
+                    <v-text-field v-model="password" label="Password" type="password" required/>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+      
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="6" sm="2">
+                    <v-btn type="submit" block class="mt-2">Login</v-btn>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+            
+    </v-form>
+    </v-container>
 </template>
 
 <style scoped>
