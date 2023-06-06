88cefc33116b8fdb494405f0ba7899dc29382840
Lou8is <lis.gpublic@gmail.com>
2023-06-06 15:20:03 +0200

    Provides 1st version of footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -4,12 +4,20 @@
 
 <template>
     <v-footer>
-        <p>footer</p>
+        
+            <v-row justify="center">
+                <v-col class="text-center mt-4" cols="12">
+                LOMR {{ new Date().getFullYear() }}<br/>
+                Everything on this blog is published under a <a href="https://creativecommons.org/licenses/by/4.0/" nofollow>Attribution 4.0 International (CC BY 4.0)</a>, so do what you want with it as long as you provide appropriate attribution.
+                </v-col>
+            </v-row>
+        
     </v-footer>
 </template>
 
 <style scoped>
     footer {
+        font-size: x-small;
         max-height: 10vh;
     }
 </style>
