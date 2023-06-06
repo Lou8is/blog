f05db0eb6763787f28fe970b8bedca35fb1bc938
Lou8is <lis.gpublic@gmail.com>
2023-06-06 15:55:07 +0200

    Adds small Home intro

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,10 +4,26 @@
 
 <template>
     
-  <p>home</p>
+  <v-container id="intro" fluid>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <p>
+          Welcome! This blog is all about the things I find interesting in the tech world, especially software and web related.
+           I'll be sharing my own views and experiences, and I hope you'll find it just as fascinating as I do.
+        </p>
+      </v-col>
+    </v-row>
 
+  </v-container>
+
+  <v-container>
+    Coming soon: display latest articles
+  </v-container>
 </template>
 
 <style scoped>
-
+  #intro {
+    background-color: #cccccccc;
+    text-align: justify;
+  }
 </style>
