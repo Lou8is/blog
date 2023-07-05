747c7e2fdc630d3cdd24ff186d2585652e3d22d8
Lou8is <lis.gpublic@gmail.com>
2023-07-05 15:20:42 +0200

    Creates article entity

--- backend/src/articles/entities/article.entity.ts
+++ backend/src/articles/entities/article.entity.ts
@@ -1,1 +1,49 @@
-export class Article {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from 'mongoose';
+
+export type ArticleDocument = HydratedDocument<Article>;
+
+@Schema()
+export class Article {
+    //Note : mongoose provide a default _id
+
+    /**
+     * Always required 
+     */
+    @Prop({ type: String, required: true })
+    title: string;
+
+    /**
+     * Always required 
+     */
+    @Prop({ type: String, required: true })
+    slug: string;
+
+    /**
+     * Article publication date
+     */
+    @Prop({ type: Date, default: Date.now()})
+    published_on: Date;
+
+    /**
+     * Article creation date
+     */
+    @Prop({ type: Date, default: Date.now()})
+    created_on: Date;
+
+    /**
+     * Article modification date
+     */
+    @Prop({ type: Date, default: Date.now()})
+    modified_on: Date;
+
+    /**
+     * Always required 
+     */
+    @Prop({ type: String, required: true })
+    content: string;
+
+
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
